804792e3b42be86961ce6e8db7b3556ba0c7d966
Abhyuday <abhyuday911@gmail.com>
2023-02-22 19:39:10 +0530

    changed particle bonds for mobile devices

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,10 @@
 // console.log(document.querySelector("#main").offsetHeight)
+
+// document.querySelector("#main")
+function isMobileDevicee() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  }
+  
+  if (isMobileDevicee()) {
+    document.querySelector("#mainelements").style.pointerEvents= "all"
+  } 
